107e3e788b8299d1da9cf934998031b7d50c5341
felipefortinif <felipefortinif@gmail.com>
2025-11-10 17:24:03 -0300

    feat: add OnInit to HeaderComponent

--- beachbuddy-app/src/app/shared/components/header/header.component.ts
+++ beachbuddy-app/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, effect } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
@@ -10,8 +10,24 @@ import { AuthService } from '../../../core/services/auth.service';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   authService = inject(AuthService);
+
+  constructor() {
+    // Effect para debug
+    effect(() => {
+      const user = this.authService.currentUser();
+      console.log('Header - User:', user);
+      console.log('Header - isAuthenticated:', this.authService.isAuthenticated());
+      console.log('Header - isAluno:', this.authService.isAluno());
+      console.log('Header - isProfessor:', this.authService.isProfessor());
+      console.log('Header - isGerente:', this.authService.isGerente());
+    });
+  }
+
+  ngOnInit(): void {
+    console.log('HeaderComponent initialized');
+  }
 
   logout(): void {
     this.authService.logout();
